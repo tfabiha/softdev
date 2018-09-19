ee4a8371f5ebcdd30b4b0eefcd69689739bf6628
tfabiha <tfabiha@stuy.edu>
2018-09-19 18:52:31 -0400

    change folder names

---
+++ 02-randomizer/Codars_GelfandD-TabassumF.py
@@ -0,0 +1,28 @@
+'''
+Codars - Daniel Gelfand and Tabassum Fabiha
+SoftDev1 pd<6>
+K<02> -- NO-body expects the Spanish Inquisition!
+2018-09-12
+'''
+
+from random import randint
+
+KREWES = {
+        'w': ['William Lu', 'Qian', 'Peter', 'Ahnaf', 'Kenny', 'Sophia', 'Sajed', 'Emily', 'Hasif', 'Brian ', 'Dennis', 'Jiayang', 'Shafali ', 'Isaac ', 'Tania', 'Derek', 'Shin', 'Vincent', 'Ricky', 'Puneet', 'Wei Wen', 'Tim', 'Jeffrey', 'Joyce', 'Mohtasim', 'Simon', 'Thomas', 'Ray', 'Jack', 'Karen', 'Robin', 'Jabir', 'Johnny ', 'Matthew', 'Johnson Li', 'Angela', 'Crystal', 'Jiajie', 'Theodore (Dont really care)', 'Anton', 'Max', 'Bo', 'Andrew', 'Kendrick', 'Kevin', 'Kyle', 'Jamil', 'Mohammed', 'Ryan', 'Jason'],
+
+        'm': ['Daniel', 'Aleksandra', 'Addison', 'Hui Min', 'Aaron', 'Rubin', 'Raunak', 'Stefan', 'Cheryl', 'Cathy', 'Mai', 'Claire ', 'Alex', 'Bill', 'Daniel', 'Jason'],
+
+        'x': ['Derek', 'Britni', 'Joan', 'Vincent', 'Jared', 'Ivan', 'Thomas', 'Maggie', 'Damian', 'Tina', 'Fabiha', 'John', 'Susan ', 'Kaitlin', 'Michelle', 'Clara', 'Rachel', 'Amit', 'Jerry', 'Raymond', 'Zane', 'Soojin', 'Maryann', 'Adil', 'Josh', 'Imad']
+}
+
+def randStudent(key):
+    #select random integer to use for index
+    randIndex = randint(0,len(KREWES[key])-1)
+    #get list of students based on the kew used
+    studList = KREWES[key]
+    #returning the student name from list
+    return studList[randIndex]
+
+print("This is student " + randStudent("w") + " of team w.")
+print("This is student " + randStudent("m") + " of team m.")
+print("This is student " + randStudent("x") + " of team x.")
