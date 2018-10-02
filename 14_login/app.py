2a60d700155aa0610dbc9436eb9c1e72d3ccb272
tfabiha <tfabiha@stuy.edu>
2018-10-01 22:16:58 -0400

    adding hw

--- 14_login/app.py
+++ 14_login/app.py
@@ -1,22 +1,58 @@
 # Frozen Cookies - T. Fabiha && Stefan Tan
+#SoftDev1 pd<p>
+#K14 -- Do I know you?
+#2018-10-02
 
+from flask import Flask, render_template, request, redirect, url_for, session
+import os
 
-from flask import Flask, render_template, request
-from util import authenticate
 app = Flask(__name__)
 
+# hardcopy of the user data
 user = "bob"
 pswd = "bobby"
 
 @app.route("/")
-def hello():
-	return render_template("login.html")
+def home():
+	login = session.get("username") == user and session.get("password") == pswd # checks to see if user is logged in correctly
+	mistake = "" # will hold the mistake messages
+	#print(login)
+
+	# sees if there needs to be mistake messages added to the site
+	# if user is not logged in but has attemped to log in then there's something wrong
+	if not login and session.get("attempted"):
+
+		# if the username is incorrect
+		if not (session.get("username") == user):
+			mistake += "Username is incorrect. "
+
+		# if the password is incorrect
+		if not (session.get("password") == pswd):
+			mistake += "Password is incorrect."
+
+	#print("Session's value of logged: ")
+	#print(session.get("logged"))
+
+	return render_template("login.html",
+							logged = login,
+							error = mistake,
+							name = session.get("username")) # name of user
 
 @app.route("/auth")
 def authorization():
-    request.cookies.get("username")
-    return render_template("authorize.html")
+	session["username"] = request.args["username"] # stores username
+	session["password"] = request.args["password"] # stores password
+	session["attempted"] = True # has the user attempted to log in
+	return redirect(url_for("home"))
+
+@app.route("/logout")
+def logout():
+	session.pop("username")
+	session.pop("password")
+	session.pop("attempted")
+	return redirect(url_for("home"))
 
 if __name__ == "__main__":
+	app.secret_key = os.urandom(32)
 	app.debug = True
 	app.run()
