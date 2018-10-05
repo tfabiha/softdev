f3d054d3118c22e996f0608d19c29c759d9068cc
tfabiha <tfabiha@stuy.edu>
2018-10-04 20:32:47 -0400

    finished hw

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -1,4 +1,4 @@
-#Scriptors
+#Scriptors -- Daniel Gelfand && T Fabiha
 #SoftDev1 pd6
 #SQLITE3 BASICS
 #2018-10-04
@@ -15,21 +15,38 @@ c = db.cursor()               #facilitate db ops
 #==========================================================
 #INSERT YOUR POPULATE CODE IN THIS ZONE
 
-#CREATING THE TABLE
+#CREATING PEEPS TABLE
 command = "CREATE TABLE peeps(name TEXT, age INTEGER, id INTEGER)"
 c.execute(command)
 
+#Opening peeps.csv and reading
 with open("peeps.csv") as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        #print(row['name'], row['age'], row['id'])
-      #  command = "INSERT INTO peeps VALUES( " + row['name'] + "," + row['age'] + "," + row['id'] + ")"
+        print(row['name'], row['age'], row['id'])
+        #Adding onto the command to incorporate names, ages, and ids in CSV file
+        command = 'INSERT INTO peeps VALUES '
+        command += '( "' + row['name'] + '", '
+        command += row['age'] + ', '
+        command += row['id'] + ')'
+        c.execute(command)
 
-# c.execute(command)
+#CREATING COURSES TABLE
+command = "CREATE TABLE courses(name TEXT, age INTEGER, id INTEGER)"
+c.execute(command)
+
+with open("courses.csv") as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(row['code'], row['mark'], row['id'])
+        #Adding onto the command to incorporate codes, marks, and ids in CSV file
+        command = 'INSERT INTO courses VALUES '
+        command += '( "' + row['code'] + '", '
+        command += row['mark'] + ', '
+        command += row['id'] + ')'
+        c.execute(command)
 
 #==========================================================
 
 db.commit() #save changes
 db.close()  #close database
-
-
